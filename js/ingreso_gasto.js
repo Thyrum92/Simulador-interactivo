2aa6e2bab6308d0322ba67bced84ea9edcbf7329
Federico De Meco <federico.demeco@hotmail.com>
2022-10-31 14:48:46 -0300

    comenzando a hacer el arreglo de clientes en ingreso_gasto.js

--- js/ingreso_gasto.js
+++ js/ingreso_gasto.js
@@ -1,22 +1,46 @@
-// Agregar gastos
+// Creacion de variables para usar en el evento de click
 
 let buscarClienteIn = document.getElementById("buscarCliente")
-let buscarIDIn = document.getElementById("buscarID")
+let buscarDescripcionIn = document.getElementById("buscarDescripcion")
 let montoIn = document.getElementById("Monto")
 let comentarioIn = document.getElementById("Comentario")
 let botonMonto = document.getElementById("botonMonto")
+
+lista_de_clientes = "listadoClientes"
+
+// creacion de arrays para la pagina de registro de monto
+
+let arregloClientes = new Array
+
+let arrayLocalC = JSON.parse(localStorage.getItem(lista_de_clientes))
+
+arrayLocalC.forEach( (i) => {
+arregloClientes.push(arrayLocalC[i].empresa)
+})
+console.log(arregloClientes)
+
+
+let arregloDescripciones = new Array
+
+let arrayLocalD = JSON.parse(localStorage.getItem(lista_de_clientes))
+
+for (i=0 ; i > arrayLocalD.lengh ; i++) {
+
+    arregloDescripciones.push(arrayLocalD[i].descripcion)
+
+}
+
+
+// Evento de click para agregar gastos
 
 botonMonto.addEventListener("click", (e) => {
 
     e.preventDefault()
 
     let buscarCliente = buscarClienteIn.value
-    let buscarID = buscarIDIn.value
+    let buscarDescripcion = buscarDescripcionIn.value
     let monto = montoIn.value
     let comentario = comentarioIn.value
-
-    validadorDatosMonto(buscarCliente,buscarID,monto,comentario)
-
 
 
 })
